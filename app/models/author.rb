c8ef9214e532e0ef7bfd00775bbda7f254c3023c
Rohit Gupta <rohgupta@bestpeers.in>
2023-10-27 18:02:11 +0530

    third coomit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,15 @@
-class Author < ApplicationRecord
-    has_many :books
-    has_many :reviews, through: :books
+# class Author < ApplicationRecord
+#     has_many :books, dependent: :destroy
+#     has_many :reviews, through: :books
 
+#     accepts_nested_attributes_for :books
+# end
+
+
+class Author < ApplicationRecord
+    has_many :books, dependent: :destroy
+    has_many :reviews, through: :books, dependent: :destroy # Add dependent option here
+  
     accepts_nested_attributes_for :books
-end
+  end
+  
